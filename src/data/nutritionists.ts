64c9721d85f9293d1c840e80546a3699415d9502
Maple！ <mapleee723@gmail.com>
2026-01-08 16:42:54 +0800

    移除廖悅辰營養師的相關資訊 (#1)

--- src/data/nutritionists.ts
+++ src/data/nutritionists.ts
@@ -16,57 +16,6 @@ import yangImage from '../assets/images/nutritionists/yang-fu-lin.png';
 
 export const NUTRITIONISTS: Nutritionist[] = [
     {
-        id: 'liao-yue-chen',
-        name: '廖悅辰',
-        title: '國家高考合格營養師',
-        shortBio: '專精於糖尿病與慢性病飲食調理，提供客製化的體重管理方案。',
-        fullBio: `廖悅辰營養師擁有豐富的臨床經驗，曾任職於台北醫學大學附設醫院及衛生所，擅長將專業的醫療營養知識轉化為日常可執行的飲食計畫。`,
-        image: '', // TODO: 替換為實際圖片路徑
-        instagram: '', // TODO: 替換為實際 IG 連結
-        credentials: [
-            {
-                icon: 'fa-certificate',
-                label: '國家高考營養師',
-                description: '中華民國專技高等考試及格',
-                highlight: true
-            },
-            {
-                icon: 'fa-user-md',
-                label: '糖尿病共照網認證',
-                description: '新北市政府衛生局認證',
-                highlight: true
-            },
-            {
-                icon: 'fa-heart-pulse',
-                label: '慢性腎病照護',
-                description: '台灣腎臟醫學會進階班認證',
-                highlight: false
-            },
-            {
-                icon: 'fa-hands-holding-circle',
-                label: '長照 Level 1',
-                description: '衛生福利部認證',
-                highlight: false
-            },
-            {
-                icon: 'fa-shield-halved',
-                label: 'HACCP 食品安全',
-                description: '衛福部食藥屬進階證照',
-                highlight: false
-            },
-            {
-                icon: 'fa-utensils',
-                label: '中餐丙級技術士',
-                description: '開出好吃又健康的餐點',
-                highlight: true
-            }
-        ],
-        specialties: ['糖尿病專科營養', '疾病飲食諮詢', '個人化體重管理'],
-        suitableFor: ['糖尿病前期或確診患者', '有三高問題的族群', '想要健康減重不復胖'],
-        quote: '飲食控制不是限制，而是為了更自由的享受生活。',
-        publicMenus: [] // TODO: 新增預設菜單
-    },
-    {
         id: 'yang-fu-lin',
         name: '楊福臨',
         title: '國家高考合格營養師 / 運動科學碩士',
